e459821a2d99be53972e08e5141a4ba8299de0e6
tengyue <tengyue@aa.com>
2023-08-21 10:38:20 +0800

    master trith  commit test

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -8,9 +8,10 @@ public class Demo1Application {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(Demo1Application.class, args);
-		System.out.println("hello git!");
+		System.out.println("hello git! master");
 		System.out.println("hello git!");
 		System.out.println("hello git!  hot-fix");
+		System.out.println("hello git!  master2");
 	}
 
 }
